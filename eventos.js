cea7c2e04f8357db5d85ec2a8a84b4774e4fa967
sergioplayxd <sergiopp098098092@gmail.com>
2026-09-13 17:18:47 +0200

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -51,8 +51,8 @@ const eventos = [
     },
 
     {
-        inicio: "14-09",
-        fin: "14-09",
+        inicio: "13-09",
+        fin: "13-09",
         titulo: "Fecha de Creacion",
         descripcion: "Hoy fue el dia en el que la pagina fue disponible para todos."
     },
